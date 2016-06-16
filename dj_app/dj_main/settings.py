2c56eaaafc57c6e21b1f7c203b0735c0459f9ba3
pogorelov-ss <pogorelov.ss@gmail.com>
2016-06-16 22:58:13 +0300

    rename envs

--- dj_app/dj_main/settings.py
+++ dj_app/dj_main/settings.py
@@ -96,11 +96,11 @@ WSGI_APPLICATION = 'dj_main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('MAIN_DB_NAME'),
-        'USER': os.environ.get('MAIN_DB_USERNAME'),
-        'PASSWORD': os.environ.get('MAIN_DB_PASSWORD'),
-        'HOST': os.environ.get('MAIN_DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('MAIN_DB_PORT', '5432')
+        'NAME': os.environ.get('RDS_DB_NAME'),
+        'USER': os.environ.get('RDS_USERNAME'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': os.environ.get('RDS_HOSTNAME'),
+        'PORT': os.environ.get('RDS_PORT')
     }
 }
 
